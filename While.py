52c33cbaa802b1e21e6059e0a2ad254cdb72418e
LeonardoArroba <86578215+LeonardoArroba@users.noreply.github.com>
2021-06-29 19:36:52 -0500

    Delete While.py

--- While.py
+++
@@ -1,6 +0,0 @@
-vocal = input("Ingrese vocal:")
-while vocal not in ("a", "e", "i", "o", "u"):
-    if vocal == '.':
-        break
-    vocal = input("Vocal:")
-print('Su vocal o punto es:{}'.format(vocal))
